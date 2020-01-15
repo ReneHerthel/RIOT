3ed9e7ff341ec93565c1b1a1f13b4b6957e5dbd8
Rene Herthel <rene.herthel@haw-hamburg.de>
2020-01-15 23:56:57 +0100

    enrollment and reconstruction done in one flash; printing the id now

--- tests/puf_sram_node/main.c
+++ tests/puf_sram_node/main.c
@@ -30,12 +30,6 @@
 #include "puf_sram.h"
 #include "periph/eeprom.h"
 
-/* Buffer used in the enrollment. */
-static uint8_t enr_helper[PUF_SRAM_HELPER_LEN] = {0};
-static uint8_t enr_bytes[PUF_SRAM_CODEOFFSET_LEN] = {0};
-static uint8_t enr_gol_enc[PUF_SRAM_CODEOFFSET_LEN] = {0};
-static uint8_t enr_rep_enc[PUF_SRAM_GOLAY_LEN] = {0};
-
 static inline void _print_buf(uint8_t *buf, size_t len, char *title)
 {
     printf("%s\n", title);
@@ -45,15 +39,16 @@ static inline void _print_buf(uint8_t *buf, size_t len, char *title)
     printf("\n- - - - - - - - - - - - - - - -\n\n");
 }
 
-/*
- * @brief   The 'enrollment' of the device. Uses the puf_sram_seed variable to
- *          access the memory pattern, which was initialized on startup in the
- *          puf_sram module. Calculate helper data with the pattern and save it
- *          on the EEPROM.
- */
-static inline void _enrollment()
+static inline void _enrollment(void)
 {
+    static uint8_t helper[PUF_SRAM_HELPER_LEN] = {0};
+    static uint8_t golay[PUF_SRAM_CODEOFFSET_LEN] = {0};
+    static uint8_t repetition[PUF_SRAM_GOLAY_LEN] = {0};
+    /* XXX: Fixed values for debug purposes. Will change later. */
+    static uint8_t bytes[PUF_SRAM_CODEOFFSET_LEN] = {1, 1, 1, 1, 1, 1};
+
     puts("_enrollment() - start");
+
     /* TODO: Add support for multiple iterations.
           - Sum up in binary representation (if needed)
           - Binarize maximum to likelihood
@@ -61,64 +56,57 @@ static inline void _enrollment()
           NOTE: The very first init of puf_sram_seed is the first iteration.
           Just hold it simple for the moment.
           dist/tools/puf-sram/puf_sram.py:56 */
-    uint8_t *enr_puf_ref = (uint8_t *)puf_sram_seed;
+    uint8_t *mle_response = (uint8_t *)puf_sram_seed;
 
-    _print_buf(enr_puf_ref, PUF_SRAM_HELPER_LEN, "enr_puf_ref:");
-
-    /* Write random bytes in range(0, 255) into enr_bytes.
+    /* Write random bytes in range(0, 255) into bytes.
        NOTE: seed generated on sys/random/random.c:49 with
-       random_init(puf_sram_seed). */
-    //random_bytes(enr_bytes, PUF_SRAM_CODEOFFSET_LEN);
+       random_init(puf_sram_seed).
+       XXX: Fixed values are used ATM. */
+    //random_bytes(bytes, PUF_SRAM_CODEOFFSET_LEN);
 
-    // 14. Jan: Fürs debuggen: statischen codeoffset benutzen. Immer die gleichen Werte
-    static uint8_t fixed_offset = {1, 1, 1, 1, 1, 1};
-    //_print_buf(enr_bytes, PUF_SRAM_CODEOFFSET_LEN, "enr_bytes:");
+    golay2412_encode(PUF_SRAM_CODEOFFSET_LEN, bytes, golay);
 
-    /* The golay encoder takes the random sample of enr_bytes. */
-    golay2412_encode(PUF_SRAM_CODEOFFSET_LEN, enr_bytes, enr_gol_enc);
-    //_print_buf(enr_gol_enc, PUF_SRAM_CODEOFFSET_LEN, "enr_gol_enc");
+    repetition_encode(PUF_SRAM_CODEOFFSET_LEN, golay, repetition);
 
-    /* The repetition encoder takes the encoded golay data. */
-    repetition_encode(PUF_SRAM_CODEOFFSET_LEN, enr_gol_enc, enr_rep_enc);
-    //_print_buf(enr_rep_enc, PUF_SRAM_CODEOFFSET_LEN, "enr_rep_enc");
-
-    /* XOR the result of the calc. repetition with the calc. MLE-response. */
     for(unsigned i = 0; i < PUF_SRAM_CODEOFFSET_LEN; i++) {
-        enr_helper[i] = enr_rep_enc[i] ^ enr_puf_ref[i];
+        helper[i] = repetition[i] ^ mle_response[i];
     }
-    //_print_buf(enr_helper, PUF_SRAM_CODEOFFSET_LEN, "enr_helper");
 
     /* Save the generated helper data to non-volatile memory.
        NOTE: For the moment, it is restricted to EEPROM. */
-    eeprom_write(PUF_SRAM_HELPER_EEPROM_START, enr_helper, PUF_SRAM_HELPER_LEN);
+    eeprom_write(PUF_SRAM_HELPER_EEPROM_START, helper, PUF_SRAM_HELPER_LEN);
 
     puts("_enrollment() - done");
 }
 
-/*
- *
- */
-static inline void _reconstruction()
+static inline void _reconstruction(void)
 {
     puts("_reconstruction() - start");
     /* Key is generated in puf_sram.c, during power cycle. */
     // TODO:
     // puf_sram_delete_secret();
+    printf(" 0x");
+    for (unsigned i=0;i<sizeof(puf_sram_id);i++){
+        printf("%x", puf_sram_id[i]);
+    }
+    printf("\n");
+    puf_sram_delete_secret();
     puts("_reconstruction() - done");
 }
 
 int main(void)
 {
-    puts("\nRIOT/tests/puf-sram-node/");
-
+/*
 #ifdef PUF_SRAM_GEN_HELPER
     _enrollment();
 #else
-
 #ifdef MODULE_PUF_SRAM_SECRET
     _reconstruction();
-#endif /* MODULE_PUF_SRAM_SECRET */
-
-#endif /* PUF_SRAM_GEN_HELPER */
+#endif
+#endif
+*/
+    puts("\nRIOT/tests/puf-sram-node/");
+    _enrollment();
+    _reconstruction();
     return 0;
 }
